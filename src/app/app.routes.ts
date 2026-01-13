bc71ee619558c1aae682eff97d428bf33521d0e8
Leonardo Tavera <leonardo.tavera.anton@outlook.com>
2026-01-13 09:53:35 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
-import { EmployeeComponent } from './components/employee/employee.component';
-import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 
-export const appRoutes: Routes = [
-  {path: '', component:EmployeeComponent},
-  {path: 'edit/:id', component:EmployeeEditComponent}
-];
+export const routes: Routes = [];
